51b9a6f574a9d348cf4c11a999288ac003912cf9
예지 <lee24pm@gmail.com>
2025-01-07 14:17:59 +0900

    init: 프로젝트 초기 설정

---
+++ src/main/java/com/example/gamemate/GameMateApplication.java
@@ -0,0 +1,13 @@
+package com.example.gamemate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GameMateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GameMateApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/gamemate/GameMateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gamemate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GameMateApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
