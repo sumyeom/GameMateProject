49bc65afbc0ac46af92822f74fe5e6080e00285f
Newbiekk <newbiekk1126@gmail.com>
2025-01-08 15:53:25 +0900

    fix : User 엔티티 수정
    1. 영속성 전이 및 고아 객체 삭제 제거
    2. 객체 기준으로 follow 엔티티에서 follower 를 맵핑 받은 List<Follow> 는 그 User 가 팔로우한 User 목록의 정보를 담고있으니 followingList 로 변경
    3. 객체 기준으로 follow 엔티티에서 followee 를 맵핑 받은 List<Follow> 는 그 User 를 팔로우한 User 목록의 정보를 담고있으니 followerList 로 변경

--- src/main/java/com/example/gamemate/domain/user/entity/User.java
+++ src/main/java/com/example/gamemate/domain/user/entity/User.java
@@ -1,5 +1,6 @@
 package com.example.gamemate.domain.user.entity;
 
+import com.example.gamemate.domain.follow.Follow;
 import com.example.gamemate.global.common.BaseEntity;
 import com.example.gamemate.domain.user.enums.Authority;
 import com.example.gamemate.domain.user.enums.UserStatus;
@@ -8,6 +9,7 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 @Entity
 @Table(name = "user")
@@ -39,6 +41,12 @@ public class User extends BaseEntity {
 
     @Enumerated(EnumType.STRING)
     private UserStatus userStatus;
+
+    @OneToMany(mappedBy = "follower")
+    private List<Follow> followingList;
+
+    @OneToMany(mappedBy = "followee")
+    private List<Follow> followerList;
 
     public User(String email, String name, String nickname, String password) {
         this.email = email;
