52d69a2166c60b993f1190972d20b108fd9e658c
예지 <lee24pm@gmail.com>
2025-01-08 17:35:43 +0900

    fix: dto 필드 final 선언

--- src/main/java/com/example/gamemate/domain/user/dto/ProfileUpdateRequestDto.java
+++ src/main/java/com/example/gamemate/domain/user/dto/ProfileUpdateRequestDto.java
@@ -2,11 +2,13 @@ package com.example.gamemate.domain.user.dto;
 
 import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 
 @Getter
+@RequiredArgsConstructor
 public class ProfileUpdateRequestDto {
 
-    @NotBlank
-    private String newNickname;
+    @NotBlank(message = "새로운 닉네임을 입력해주세요.")
+    private final String newNickname;
 
 }
