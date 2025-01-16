5b4790d3f7f2e627ab9c62ae6eeee15f691267ac
Newbiekk <newbiekk1126@gmail.com>
2025-01-16 20:34:03 +0900

    feat : NotificationType 항목 추가

--- src/main/java/com/example/gamemate/domain/notification/enums/NotificationType.java
+++ src/main/java/com/example/gamemate/domain/notification/enums/NotificationType.java
@@ -4,10 +4,12 @@ import lombok.Getter;
 
 @Getter
 public enum NotificationType {
-    FOLLOW("follow", "새 팔로워가 생겼습니다."),
-    COMMENT("comment", "새로운 댓글이 달렸습니다."),
-    MATCHING("matching", "매칭에 성공했습니다."),
-    LIKE("like", "좋아요가 달렸습니다.");
+    NEW_FOLLOWER("follow", "새 팔로워가 생겼습니다."),
+    NEW_COMMENT("comment", "새로운 댓글이 달렸습니다."),
+    NEW_MATCH("new_match", "새로운 매칭 요청이 왔습니다."),
+    MATCH_REJECTED("match_rejected", "보낸 매칭이 거절되었습니다."),
+    MATCH_ACCEPTED("match_accepted", "보낸 매칭이 수락되었습니다."),
+    NEW_LIKE("like", "새 좋아요가 달렸습니다.");
 
     private final String name;
     private final String content;
