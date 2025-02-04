d3dda5c09a77bdb8e8c9aef92b41a4f619de5603
sumyeom <wjstndus75@naver.com>
2025-02-04 19:45:56 +0900

    build: docker-compose.yml, deploy.sh 파일 수정

--- deploy.sh
+++ deploy.sh
@@ -11,11 +11,10 @@ ECR_URI="$AWS_ACCOUNT_ID.dkr.ecr.$REGION.amazonaws.com/$ECR_REPOSITORY"
 aws ecr get-login-password --region $REGION | docker login --username AWS --password-stdin $ECR_URI
 
 # 최신 이미지 Pull
-docker pull $ECR_URI:$IMAGE_TAG
+sudo docker pull $ECR_URI:$IMAGE_TAG
 
 # 기존 컨테이너 중지 및 삭제
-docker stop app || true
-docker rm app || true
+sudo docker-compose down || true
 
 # docker-compose 실행 (환경변수 적용)
-docker-compose -f /home/ubuntu/docker-compose.yml up -d
+sudo docker-compose -f /home/ubuntu/docker-compose.yml up -d
