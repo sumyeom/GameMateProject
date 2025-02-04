6c2020a481948d39fadc6e641eb0e220f0f1858e
sumyeom <wjstndus75@naver.com>
2025-02-04 17:20:41 +0900

    build: deploy.sh 추가, github-actions.yml 파일 수정
    1. deploy.sh 추가
    2. github-actions.yml 파일 수정

---
+++ deploy.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# ECR 설정
+AWS_ACCOUNT_ID="863518453426"
+REGION="ap-northeast-2"
+ECR_REPOSITORY="game_mate"
+IMAGE_TAG="latest"
+ECR_URI="$AWS_ACCOUNT_ID.dkr.ecr.$REGION.amazonaws.com/$ECR_REPOSITORY"
+
+# AWS ECR 로그인
+aws ecr get-login-password --region $REGION | docker login --username AWS --password-stdin $ECR_URI
+
+# 최신 이미지 Pull
+docker pull $ECR_URI:$IMAGE_TAG
+
+# 기존 컨테이너 중지 및 삭제
+docker stop spring-app || true
+docker rm spring-app || true
+
+# docker-compose 실행 (환경변수 적용)
+docker-compose -f /home/ec2-user/docker-compose.yml up -d
