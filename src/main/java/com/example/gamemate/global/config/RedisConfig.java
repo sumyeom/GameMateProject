854f22a15dc9c56e195f5fd84fb3b640672e7148
sumyeom <wjstndus75@naver.com>
2025-02-07 00:13:40 +0900

    fix: 환경 변수 추가

--- src/main/java/com/example/gamemate/global/config/RedisConfig.java
+++ src/main/java/com/example/gamemate/global/config/RedisConfig.java
@@ -3,6 +3,7 @@ package com.example.gamemate.global.config;
 import org.redisson.Redisson;
 import org.redisson.api.RedissonClient;
 import org.redisson.config.Config;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
@@ -16,6 +17,9 @@ import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
 public class RedisConfig {
+
+    @Value("${spring.data.redis.host}")
+    private String redisHost;
 
     // 기본 RedisConnectionFactory
     @Bean
